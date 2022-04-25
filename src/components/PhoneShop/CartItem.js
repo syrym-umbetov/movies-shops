b5c12b4043854552f6239ea770c0f1eb6445e1c6
syrym-umbetov <umbetov.syrym@gmail.com>
2022-04-25 23:05:29 +0600

    count price on reduxtoolkit

--- src/components/PhoneShop/CartItem.js
+++ src/components/PhoneShop/CartItem.js
@@ -14,7 +14,7 @@ const CartItem = ({ id, img, title, price, amount }) => {
       <img src={img} alt={title} />
       <div>
         <h4>{title}</h4>
-        <h4 className='item-price'>${price}</h4>
+        <h4 className='item-price'>${(price * amount).toFixed(2)}</h4>
         <button
           className='remove-btn'
           onClick={() => {
